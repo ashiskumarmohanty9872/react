0f981ab4dc0b9b296428ccb4571a1e2203109a58
ashiskumarmohanty9872 <144931369+ashiskumarmohanty9872@users.noreply.github.com>
2024-12-26 19:23:42 +0530

    commit this

--- 01vitreact/src/App.jsx
+++ 01vitreact/src/App.jsx
@@ -1,10 +1,10 @@
 import Chai from "./chai"
 function App() {
-
+  const userName = "Sibun"
   return (
     <>
     <Chai/>
-    <h1>Chai aur code</h1>
+    <h1>Chai aur code and {userName}</h1>
     <p>Hii sibun </p></>
     
   )
--- 01vitreact/src/main.jsx
+++ 01vitreact/src/main.jsx
@@ -1,8 +1,80 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp(){
+    return(
+        <div>
+            <h1> Custom render ! chai</h1>
+        </div>
+    )
+}
+
+
+// global varibale declare kariba dekiba kemti Evaluate edpresssion kama karuchi
+
+const  otherUser= "Sibun is a hard worker"
+
+// Custom react code
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.youtube.com/watch?v=kAOuj6o7Kxs&t=1418s',
+        target: '_blank',
+       
+    },
+    children: 'Click me to visit Chai Aur Code'
+}
+
+// object 
+const anotherElement = (
+    <a href= "https://www.youtube.com/watch?v=kAOuj6o7Kxs&t=1418s" target='_blank'>Cahi aur code</a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://www.youtube.com/watch?v=kAOuj6o7Kxs&t=1418s' ,  target: '_blank'},
+    'click me to visit chai aur code',
+    otherUser
+)
+
 
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    //  < App /> //-> (1)
+
+    // <MyApp /> //-> (2)
+
+    // MyApp()  -> (3)
+
+    // anotherElement //object re convert houchi (4)
+
+     reactElement // 5) react element jete bele create kale aeita pass  kriba
  
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Git command :-
+// 1) git add -A
+// 2) git commit -m 'commit this'
+// 3) git push
